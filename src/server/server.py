2d3cf9ff37e87ed74a0089fe384682f0cc585ee1
sebastian11111111 <107846120+sebastian11111111@users.noreply.github.com>
2023-01-19 08:27:10 +0100

    Feature: Server can handle multiple games and dont need to be restarted every time

--- src/server/server.py
+++ src/server/server.py
@@ -6,8 +6,7 @@ from _thread import start_new_thread
 import sys
 import random
 import string
-
-from src.game import game
+from copy import copy
 
 
 def get_random_ids(number_of_ids, length):
@@ -25,7 +24,7 @@ def get_random_ids(number_of_ids, length):
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 number_of_games_at_a_time = 1
-number_of_players_per_game = 4
+number_of_players_per_game = 3
 server = 'localhost'
 port = 5555
 
@@ -37,32 +36,40 @@ try:
 except socket.error as e:
     print(str(e))
 
+
 s.listen(number_of_games_at_a_time * number_of_players_per_game)
 print("Waiting for a connection")
 currentGId, currentPId = 0, 0
-config_file = os.path.abspath(os.path.dirname()) + r'\configuration.json'
+config_file = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', r'game\\configuration.json'))
 with open(config_file) as file:
     game_data = json.load(file)
 
-game_datas = [game_data] * number_of_games_at_a_time
-players_connected = [[False] * number_of_players_per_game] * number_of_games_at_a_time
+game_datas = list()
+for _ in range(number_of_games_at_a_time):
+    game_datas.append(copy(game_data))
+# 0 = not connected, 1 = connected, 2 = connection lost
+players_connected = [[0] * number_of_players_per_game] * number_of_games_at_a_time
 # to convert list of game data and ids to dict
 game_data_dict = dict(zip(get_random_ids(number_of_games_at_a_time, 4), game_datas))
 
-
 def threaded_client(conn):
-    global currentGId, currentPId, game_data_dict, players_connected
-    conn.send(str.encode(str(currentGId) + ',' + str(currentPId)))
-    game_id = game_data_dict.keys()[str(currentGId)]
-    game_data_dict[game_id][str(currentPId)]["connected"] = True
-    # game_data[str(currentPId)]["connected"] = True
+    global currentGId, currentPId, game_data_dict, players_connected, game_data
+    # send realtive game and player id
+    this_gid = copy(currentGId)
+    this_pid = copy(currentPId)
+    conn.send(str.encode(str(this_gid) + ',' + str(this_pid)))
+    # get real game_id
+    game_id = list(game_data_dict.keys())[this_gid]
+    # set player status in game_data to online
+    game_data_dict[game_id][str(this_pid)]["connected"] = True
+    players_connected[this_gid][this_pid] = 1
+    # increase player id and if necessary game id for the next connection
     currentPId += 1
     if currentPId == number_of_players_per_game:
         currentGId += 1
         if currentGId == number_of_games_at_a_time:
             currentGId = 0
         currentPId = 0
-    players_connected[currentGId][currentPId] = True
     reply = ''
     last_msg = datetime.datetime.now()
     while True:
@@ -73,26 +80,32 @@ def threaded_client(conn):
                 conn.send(str.encode("Goodbye"))
                 break
             else:
-                #print("Recieved: " + reply)
                 jreply = json.loads(reply)
                 game_data_dict[game_id][str(jreply["id"])] = jreply
-
                 reply = json.dumps(game_data_dict[game_id])
-                #print("Sending: " + reply)
+
             conn.sendall(str.encode(reply))
+            last_msg = datetime.datetime.now()
         except:
             break
-        if datetime.datetime.now() - last_msg > 5:
-            print("Connection lost")
+        if (datetime.datetime.now() - last_msg).seconds > 5:
+            print("Connection timeout")
             break
 
     print("Connection Closed")
-    game_data_dict[str(currentGId)][str(currentPId)]["connected"] = None
+    #set lost player to connected false and disconected
+    game_data_dict[game_id][str(this_pid)]["connected"] = True
+    players_connected[this_gid][this_pid] = 2
     conn.close()
     # check if there is a game where every player connected once but disconnected again, if true reset the game
-    for g in players_connected:
-        if g.count(None) == number_of_players_per_game:
-            g = [False] * number_of_players_per_game
+    if players_connected[this_gid].count(2) == number_of_players_per_game:
+        players_connected[this_gid] = [0] * number_of_players_per_game
+        #print(game_data)
+        game_data_dict[game_id] = copy(game_data)
+        for n in range(number_of_players_per_game):
+            game_data_dict[game_id][str(n)]["connected"] = False
+        print("game zurückgesetzt da alle spieler diconnected sind")
+
 
 
 while True:
