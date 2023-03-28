204a968650d9efed59d0d34846a73de7c891243a
sebastian11111111 <107846120+sebastian11111111@users.noreply.github.com>
2023-03-28 15:28:31 +0200

    Fix: Server without extrathread

--- src/game/server/server.py
+++ src/game/server/server.py
@@ -262,7 +262,7 @@ def threaded_client(conn):
             exit(1)
         # get real game_id
         game_id = list(game_data_dict.keys())[this_gid]
-        start_new_thread(game_server, (game_id, this_gid,))
+        # start_new_thread(game_server, (game_id, this_gid,))
         game_data_dict[game_id]["metadata"]["map"] = start_msg
         game_data_dict[game_id]["metadata"]["spawnpoints"] = this_spawn_points
         maps_dict[game_id] = start_msg
